6329f39133c0793b9982afa2cb0c05c868b7245d
Ben McGraw <ben.mcgraw@gmail.com>
2015-11-22 19:20:03 -0800

    Enable cut/copy/paste

--- app/background.js
+++ app/background.js
@@ -41,6 +41,34 @@ app.on('ready', function () {
         mainWindow.openDevTools();
     }
 
+    var buildMainMenu = function() {
+        var Menu = require("menu");
+        
+        // Create the Application's main menu
+        var template = [{
+            label: "Application",
+            submenu: [
+                { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+                { type: "separator" },
+                { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+            ]}, {
+            label: "Edit",
+            submenu: [
+                { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+                { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+                { type: "separator" },
+                { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+                { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+                { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            ]}
+        ];
+
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    };
+    buildMainMenu();
+
+
     mainWindow.on('close', function () {
         mainWindowState.saveState(mainWindow);
     });
