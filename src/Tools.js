8b74d18f375ff959784ab075b7e3e2d51b8f166a
Ben McGraw <mcgrue@verge-rpg.com>
2016-12-23 00:44:49 -0800

    Extract Drag from Tools

--- src/Tools.js
+++ src/Tools.js
@@ -6,6 +6,7 @@ import eyedropperGenerator from './tools/Eyedropper';
 import drawGenerator from './tools/Draw';
 import selectGenerator from './tools/Select';
 import floodFillGenerator from './tools/FloodFill';
+import dragGenerator from './tools/Drag';
 
 export const updateLocationFunction = (map) => {
   const x = map.camera[0];
@@ -119,8 +120,6 @@ export const getTXTyFromMouse = (map, evt) => {
   return [tX, tY];
 };
 
-
-
 export const selectAll = (map) => {
   _toolLogic.SELECT.isSelecting = true;
   _toolLogic.SELECT.isButtonDown = false;
@@ -138,36 +137,7 @@ export const selectAll = (map) => {
 // TODO once we know the common verbs and nouns for palette tools, we really should extract each one into its own file
 // TODO and test the bejeezus out of them.  There's some janky-ass jank in here!
 export const _toolLogic = {
-  'DRAG': {
-    'dragging': false,
-    'last_mouse': [0, 0],
-
-    'mousedown': function (map, e) {
-      _toolLogic.DRAG.dragging = true;
-      window.$MAP_WINDOW.draggable('disable');
-      _toolLogic.DRAG.last_mouse = [ e.clientX, e.clientY ];
-    },
-    'mousemove': function (map, e) {
-      if (_toolLogic.DRAG.dragging) {
-        map.camera[0] += (_toolLogic.DRAG.last_mouse[0] - e.clientX) * map.camera[2];
-        map.camera[1] += (_toolLogic.DRAG.last_mouse[1] - e.clientY) * map.camera[2];
-        _toolLogic.DRAG.last_mouse = [ e.clientX, e.clientY ];
-      }
-    },
-    'mouseup': function (map, e) {
-      _toolLogic.DRAG.dragging = false;
-      map.updateLocationFn(map);
-      window.$MAP_WINDOW.draggable('enable');
-    },
-
-    'button_element': '#btn-tool-drag',
-    'human_name': 'Drag'
-    /*,
-    "mousewheel": function(map, e) {
-        zoomFn(map, e, e.originalEvent.deltaY < 0);
-    }*/
-  },
-
+  'DRAG': dragGenerator(),
   'FLOOD': floodFillGenerator(),
 
   'SMART-EYEDROPPER': {
---
+++ src/tools/Drag.js
@@ -0,0 +1,33 @@
+import { _toolLogic } from '../Tools';
+
+export default () => {
+  return {
+    'dragging': false,
+    'last_mouse': [0, 0],
+
+    'mousedown': function (map, e) {
+      _toolLogic.DRAG.dragging = true;
+      window.$MAP_WINDOW.draggable('disable');
+      _toolLogic.DRAG.last_mouse = [ e.clientX, e.clientY ];
+    },
+    'mousemove': function (map, e) {
+      if (_toolLogic.DRAG.dragging) {
+        map.camera[0] += (_toolLogic.DRAG.last_mouse[0] - e.clientX) * map.camera[2];
+        map.camera[1] += (_toolLogic.DRAG.last_mouse[1] - e.clientY) * map.camera[2];
+        _toolLogic.DRAG.last_mouse = [ e.clientX, e.clientY ];
+      }
+    },
+    'mouseup': function (map, e) {
+      _toolLogic.DRAG.dragging = false;
+      map.updateLocationFn(map);
+      window.$MAP_WINDOW.draggable('enable');
+    },
+
+    'button_element': '#btn-tool-drag',
+    'human_name': 'Drag'
+    /*,
+    "mousewheel": function(map, e) {
+        zoomFn(map, e, e.originalEvent.deltaY < 0);
+    }*/
+  };
+};
