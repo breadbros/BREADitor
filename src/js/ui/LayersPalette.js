7ac913d03ab073dca7a5f978809c251d1a193be8
Ben McGraw <ben.mcgraw@gmail.com>
2018-06-06 19:05:04 -0700

    WIP - verified the breakpoint for injection; do not commit this

--- src/js/ui/LayersPalette.js
+++ src/js/ui/LayersPalette.js
@@ -1091,6 +1091,11 @@ const update_layer = (dialog, layer_id, onComplete) => {
     vsp: vsp
   };
 
+  if( layer.dimensions.X !== layers[layer_id].dimensions.X || layer.dimensions.Y !== layers[layer_id].dimensions.Y ) {
+    alert('OMG LAYER SIZE CHANGING!!!2');
+    throw "lol no";
+  }
+
   if (layer_id === layers.length) {
     layers.push(layer);
     const layersLength = layers.length;
