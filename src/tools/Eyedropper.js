e1fd7eed2be33412c194bf164605710be362b223
Ben McGraw <ben.mcgraw@gmail.com>
2020-05-07 21:07:12 -0700

    Make dblclick on eyedropper mode open the zone or entity dialog (if on those layers)

--- src/tools/Eyedropper.js
+++ src/tools/Eyedropper.js
@@ -5,20 +5,21 @@ import { setActiveZone, scrollZonePalletteToZone } from '../js/ui/ZonesPalette';
 
 import { getSelectedLayer } from '../js/ui/LayersPalette';
 
+import { show_edit_zone_dialog } from '../js/ui/ZonesPalette';
+import { show_edit_entity_dialog } from '../js/ui/EntityPalette';
+
 export default () => {
   return {
     'mousemove': () => {},
     'mousedown': function (map, e) {
-      console.log('EYEDROPPER->mousedown...');
-
       if (!getSelectedLayer()) {
-        console.log('You havent selected a layer yet.');
+        console.error('You havent selected a layer yet.');
         window.alert('You havent selected a layer yet.');
         return;
       }
 
       if (!(e.button === 0)) {
-        console.log("Unknown eyedropper button: we know left/right (0/2), got: '" + e.button + "'.");
+        console.warn("Unknown eyedropper button: we know left/right (0/2), got: '" + e.button + "'.");
         return;
       }
 
@@ -29,17 +30,16 @@ export default () => {
       const tX = clickSet[0];
       const tY = clickSet[1];
 
-      // const doVSPselector = (tX, tY, map) => {
-      //   map.selection.deselect();
-      //   map.selection.add(tX, tY, 1, 1);
-      // };
-
       // TODO: using a valid integer as a sentinel is stupid. using sentinels is stupid. you're stupid, grue.
       if (getSelectedLayer().map_tileData_idx > 900) {
         switch (getSelectedLayer().map_tileData_idx) {
           case 999:
             zIdx = map.getZone(tX, tY);
-            console.log('ZONES!: ' + zIdx);
+            
+            if(zIdx === undefined) {
+              zIdx = 0;
+            }
+
             setActiveZone(zIdx);
 
             scrollZonePalletteToZone(zIdx);
@@ -47,7 +47,6 @@ export default () => {
             return;
           case 998:
             console.log('OBS!');
-            // doVSPselector(tX, tY, map);
             tIdx = map.getTile(tX, tY, getSelectedLayer().map_tileData_idx);
             break;
 
@@ -72,6 +71,42 @@ export default () => {
       }
 
       setTileSelectorUI('#left-palette', tIdx, map, 0, getSelectedLayer().layer.vsp);
+    },
+    'dblclick': (map, e) => {
+
+      const clickSet = getTXTyFromMouse(map, e);
+      const tX = clickSet[0];
+      const tY = clickSet[1];
+
+      let tIdx = null;
+      let zIdx = -1;
+
+      if (getSelectedLayer().map_tileData_idx > 900) {
+        switch (getSelectedLayer().map_tileData_idx) {
+          case 999:
+            zIdx = map.getZone(tX, tY);
+            if(zIdx === undefined) {
+              zIdx = 0;
+            }
+
+            setActiveZone(zIdx);
+            scrollZonePalletteToZone(zIdx);
+            show_edit_zone_dialog(zIdx);
+            return;
+
+          case 997:
+            const ent = checkEntities(map.entities['Entity Layer (E)'], null, map, clickSet);
+            if (ent) {
+              doEntitySelection(ent);
+              show_edit_entity_dialog(ent.eIdx);
+              return;
+            }
+            break;
+
+          default:
+            throw new Error('SOMETHING IS TERRIBLYH WRONG WITH A TERLKNDSHBLE SENTINEL AND GRUE IS A BAD MAN');
+        }
+      }
     },
     'button_element': '#btn-tool-eyedropper',
     'human_name': 'Eyedropper'
