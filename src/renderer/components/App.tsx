0198c900fe0249a239a71d9b2a0d5cd572bf61ca
Dan Fessler <fezzinate@gmail.com>
2022-02-10 04:36:46 -0800

    .

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -2,74 +2,9 @@ import * as React from 'react';
 import Dockable from '../../../../react-dockable/src';
 import './css/App.css';
 import { oldBootstrap } from '../../old_bootstrap.js';
+import initialState from './initialState.js';
 
 oldBootstrap();
-
-const initialState = {
-  panels: [
-    {
-      windows: [
-        {
-          selected: 0,
-          widgets: ['tool-palette'],
-          minSize: 34
-        }
-      ],
-      size: 137.5,
-      minSize: 48,
-      maxSize: 0,
-      resize: 'stretch'
-    },
-    {
-      windows: [
-        {
-          selected: 0,
-          widgets: ['map-palette'],
-          minSize: 34,
-          size: 1357,
-          maxSize: 0,
-          resize: 'stretch'
-        }
-      ],
-      size: 2046,
-      minSize: 48,
-      maxSize: 0,
-      resize: 'stretch'
-    },
-    {
-      windows: [
-        {
-          selected: 0,
-          widgets: ['info-palette'],
-          minSize: 34,
-          size: 422.5,
-          maxSize: 0,
-          resize: 'stretch'
-        },
-        {
-          selected: 0,
-          widgets: ['zones-palette'],
-          minSize: 34,
-          size: 360.40625,
-          maxSize: 0,
-          resize: 'stretch'
-        },
-        {
-          selected: 0,
-          widgets: ['layers-palette'],
-          size: 568.09375,
-          minSize: 34,
-          maxSize: 0,
-          resize: 'stretch'
-        }
-      ],
-      size: 370.5,
-      minSize: 48,
-      maxSize: 0,
-      resize: 'stretch'
-    }
-  ]
-};
 
 function App() {
   const hiddenRef = React.useRef<HTMLDivElement>(null);
---
+++ src/renderer/components/initialState.js
@@ -0,0 +1,65 @@
+export default {
+  panels: [
+    {
+      windows: [
+        {
+          selected: 0,
+          widgets: ['tool-palette'],
+          minSize: 34
+        }
+      ],
+      size: 137.5,
+      minSize: 48,
+      maxSize: 0,
+      resize: 'stretch'
+    },
+    {
+      windows: [
+        {
+          selected: 0,
+          widgets: ['map-palette'],
+          minSize: 34,
+          size: 1357,
+          maxSize: 0,
+          resize: 'stretch'
+        }
+      ],
+      size: 2046,
+      minSize: 48,
+      maxSize: 0,
+      resize: 'stretch'
+    },
+    {
+      windows: [
+        {
+          selected: 0,
+          widgets: ['info-palette'],
+          minSize: 34,
+          size: 422.5,
+          maxSize: 0,
+          resize: 'stretch'
+        },
+        {
+          selected: 0,
+          widgets: ['zones-palette'],
+          minSize: 34,
+          size: 360.40625,
+          maxSize: 0,
+          resize: 'stretch'
+        },
+        {
+          selected: 0,
+          widgets: ['layers-palette'],
+          size: 568.09375,
+          minSize: 34,
+          maxSize: 0,
+          resize: 'stretch'
+        }
+      ],
+      size: 370.5,
+      minSize: 48,
+      maxSize: 0,
+      resize: 'stretch'
+    }
+  ]
+};
